2bc36207846c0136f80feef3fb8f37a7fa97266c
Michal Daszkiewicz <m.daszkiewicz@tvn.pl>
2018-02-19 11:03:04 +0100

    changed all output lines to english

--- service-checker.sh
+++ service-checker.sh
@@ -12,9 +12,10 @@ DATFILE=io_${1}.dat
 EMAILTO=addres@provider
 EMAILFROM=address@provider
 
+# below code needs to bechanged to support other mtas, leaving as-is for now
 function send_mail
 {
-	ssmtp ${EMAILFROM} <<.
+    ssmtp ${EMAILFROM} <<.
     To: ${EMAILTO}
     Subject: ${1} problem
 
@@ -26,19 +27,19 @@ function do_check
 {
 	cmp -s /var/tmp/${DATFILE} <(echo $STAT_IO)
 	if [ "$(echo $?)" == "0" ]; then
-		echo "$DATA ${1} bez zmian, restartuje proces"
+		echo "$DATA ${1} did not change, restarting process"
 		kill $PID && /etc/init.d/${1} restart
 		sleep 2
 		if [ -z $(pgrep -f "/usr/sbin/${1}") ]; then
-			echo "$DATA Brak procesu ${1} po restarcie daemona"
-			send_mail "$DATA Brak procesu ${1} po restarcie daemona!"
+			echo "$DATA no process ${1} after daemon restart"
+			send_mail "$DATA no process ${1} after daemon restart!"
 			exit 1
 		fi
 		MODIFY_T=$(stat -c %Z /var/tmp/${DATFILE})
 		PRESENT_T=$(date +%s)
 		if [ $(($PRESENT_T - $MODIFY_T)) -le 400 ]; then
-			echo "$DATA 5 min temu proces ${1} byl restartowany!"
-			send_mail "$DATA 5 min temu proces ${1} byl restartowany!"
+			echo "$DATA 5 minutes ago ${1} was restarted!"
+			send_mail "$DATA 5 minutes ago ${1} was restarted!"
 		fi
 		echo $STAT_IO > /var/tmp/$DATFILE
 		exit 0
@@ -51,7 +52,7 @@ function do_check
 PID=$(pgrep -f "${1}"|head -1)
 echo "PID: $PID"
 if [ -z "$PID" ]; then
-	echo "$DATA Brak procesu ${1}. Exit"
+	echo "$DATA No process ${1}. Exit"
 	exit 1
 fi
 
